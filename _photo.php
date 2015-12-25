1faa59c59c6d4e21d1fc97ddba9e48d8b4ee81a1
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-12-25 09:57:24 +0200

    Add basic photo site

---
+++ _photo.php
@@ -0,0 +1,3 @@
+<a class="img-container" href="<?php echo $image; ?>" data-lightbox="test">'
+  <img src="<?php echo $image; ?>" />
+</a>'
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="/css/photos.css">
+    <link href="/css/lightbox.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      Select image to upload:
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
+
+    <hr />
+
+    <?php
+      $dirname = "photos/";
+      $images = glob($dirname."*.{gif,png,jpg,jpeg}", GLOB_BRACE);
+      foreach($images as $image) {
+        include("_photo.php");
+      }
+    ?>
+    <script src="/js/lightbox-plus-jquery.min.js"></script>
+    <script>
+      lightbox.option({
+        'resizeDuration': 100,
+        'wrapAround': true,
+        'fitImagesInViewport': true,
+      })
+    </script>
+  </body>
+</html>
